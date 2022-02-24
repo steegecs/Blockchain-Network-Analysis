4abc0e0db51ed212633a0bcbdfafb7a4e7c8eb34
Chris Steege <steegecas@gmail.com>
2022-02-24 17:03:35 -0500

    Fixed grammar stuff

---
+++ config.py
@@ -0,0 +1,6 @@
+#Input root, API_keu, and infura url
+root = "C:/Users/15138/Documents/Github Programs/"
+csv_file = "/tokenomics/Gitcoin_Donations_R7.csv"
+
+API_key = '4RHGY82N5PA5E2HW4AAP2CNV6B24RXGNNN'
+infura_url = "https://mainnet.infura.io/v3/fcff21af8f48490993576d56a6a57453"
